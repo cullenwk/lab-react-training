eac67b8702402b56c5af360a4024a73bf4c0bb16
cullenwk <92733382+cullenwk@users.noreply.github.com>
2021-12-05 23:04:18 +0100

    half done

---
+++ src/components/Like.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+
+function Like() {
+    // useState() returns us an array with two elements
+    // the first is the element we want to use in our component that rerenders when updated
+    // the second is a function that updates that element
+    
+    let [count, setCount] = useState(0)
+
+    function handleClick(event){
+        setCount(count+1)
+       
+    }
+    
+    let colors = ['purple','blue','green','yellow','orange','red']
+    let randomColor = colors[Math.floor(Math.random()* colors.length)]
+    return (
+    <button style= {{backgroundColor: randomColor}} onClick={handleClick}>
+        Like {count}
+    </button>
+     
+    );
+  }
+  
+  export default Like;
+  
